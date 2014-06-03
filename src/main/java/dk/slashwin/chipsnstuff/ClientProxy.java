6eac90d57d8e029db8ea311e84fbafb212eb602a
Jesper Jensen <delusionalLogic@gmail.com>
2014-06-03 15:55:34 +0200

    Fixed proxies

--- src/main/java/dk/slashwin/chipsnstuff/ClientProxy.java
+++ src/main/java/dk/slashwin/chipsnstuff/ClientProxy.java
@@ -1,15 +1,8 @@
 package dk.slashwin.chipsnstuff;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.client.registry.RenderingRegistry;
 import dk.slashwin.chipsnstuff.renderer.TESRChip;
 import dk.slashwin.chipsnstuff.tileentity.TEChip;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.item.Item;
-import net.minecraft.world.World;
-import net.minecraftforge.client.MinecraftForgeClient;
-import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy
 {
@@ -17,17 +10,5 @@ public class ClientProxy extends CommonProxy
 	public void registerTESR()
 	{
 		ClientRegistry.bindTileEntitySpecialRenderer(TEChip.class, new TESRChip());
-	}
-
-	@Override
-	public World getWorld(int dimId)
-	{
-		return Minecraft.getMinecraft().theWorld;
-	}
-
-	@Override
-	public boolean isClient()
-	{
-		return true;
 	}
 }
--- src/main/java/dk/slashwin/chipsnstuff/ServerProxy.java
+++ src/main/java/dk/slashwin/chipsnstuff/ServerProxy.java
@@ -1,20 +1,5 @@
 package dk.slashwin.chipsnstuff;
 
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.World;
-import net.minecraftforge.common.DimensionManager;
-
 public class ServerProxy extends CommonProxy
 {
-	@Override
-	public World getWorld(int dimId)
-	{
-		return DimensionManager.getWorld(dimId);
-	}
-
-	@Override
-	public boolean isClient()
-	{
-		return false;
-	}
 }
